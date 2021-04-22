ea928af31fb3b598db543801a72134dd903f6d99
bastolatanuja <tanujabastola143@gmail.com>
2021-04-22 13:24:04 +0545

    making of calculator

--- main.py
+++ main.py
@@ -45,6 +45,12 @@ def button_div():
     result='div'
     f_num=int(first_number)
     e.delete(0,END)
+def button_percentage():
+    first_number=e.get()
+    global f_num
+    global result
+    result='percentage'
+    e.delete(0,END)
 
 def button_equal():
     second_number=e.get()
@@ -57,23 +63,26 @@ def button_equal():
         e.insert(0, f_num * int(second_number))
     if result=='div':
         e.insert(0, f_num /int(second_number))
+    if result=='percentage':
+        e.insert(0,f_num%int(second_number))
 
-button_1=Button(root,text="1",padx=65,pady=20,fg='purple',command=lambda:button_click(1))
-button_2=Button(root,text="2",padx=65,pady=20,fg='purple',command=lambda:button_click(2))
-button_3=Button(root,text="3",padx=65,pady=20,fg='purple',command=lambda:button_click(3))
-button_4=Button(root,text="4",padx=65,pady=20,fg='red',command=lambda:button_click(4))
-button_5=Button(root,text="5",padx=65,pady=20,fg='red',command=lambda:button_click(5))
-button_6=Button(root,text="6",padx=65,pady=20,fg='red',command=lambda:button_click(6))
-button_7=Button(root,text="7",padx=65,pady=20,fg='green',command=lambda:button_click(7))
-button_8=Button(root,text="8",padx=65,pady=20,fg='green',command=lambda:button_click(8))
-button_9=Button(root,text="9",padx=65,pady=20,fg='green',command=lambda:button_click(9))
-button_0=Button(root,text="0",padx=65,pady=20,fg='green',command=lambda:button_click(0))
-button_add=Button(root,text="+",padx=65,pady=20,fg='blue',command=button_add)
-button_sub=Button(root,text="-",padx=65,pady=20,fg='blue',command=button_sub)
-button_mul=Button(root,text="*",padx=65,pady=20,fg='blue',command=button_mul)
-button_div=Button(root,text="/",padx=65,pady=20,fg='blue',command=button_div)
-button_equal=Button(root,text="=",padx=65,pady=20,fg='blue',bg='grey',command=button_equal)
-button_clear=Button(root,text="Clear",padx=80,pady=20,bg='red',command=button_clear)
+button_1=Button(root,text="1",padx=65,pady=20,bg='grey',fg='purple',font=("arial",10,"bold"),command=lambda:button_click(1))
+button_2=Button(root,text="2",padx=65,pady=20,bg='grey',fg='purple',font=("arial",10,"bold"),command=lambda:button_click(2))
+button_3=Button(root,text="3",padx=65,pady=20,bg='grey',fg='purple',font=("arial",10,"bold"),command=lambda:button_click(3))
+button_4=Button(root,text="4",padx=65,pady=20,bg='grey',fg='red',font=("arial",10,"bold"),command=lambda:button_click(4))
+button_5=Button(root,text="5",padx=65,pady=20,bg='grey',fg='red',font=("arial",10,"bold"),command=lambda:button_click(5))
+button_6=Button(root,text="6",padx=65,pady=20,bg='grey',fg='red',font=("arial",10,"bold"),command=lambda:button_click(6))
+button_7=Button(root,text="7",padx=65,pady=20,bg='grey',fg='green',font=("arial",10,"bold"),command=lambda:button_click(7))
+button_8=Button(root,text="8",padx=65,pady=20,bg='grey',fg='green',font=("arial",10,"bold"),command=lambda:button_click(8))
+button_9=Button(root,text="9",padx=65,pady=20,bg='grey',fg='green',font=("arial",10,"bold"),command=lambda:button_click(9))
+button_0=Button(root,text="0",padx=65,pady=20,bg='grey',fg='green',font=("arial",10,"bold"),command=lambda:button_click(0))
+button_add=Button(root,text="+",padx=65,pady=20,bg='grey',fg='blue',font=("arial",10,"bold"),command=button_add)
+button_sub=Button(root,text="-",padx=65,pady=20,bg='grey',fg='blue',font=("arial",10,"bold"),command=button_sub)
+button_mul=Button(root,text="*",padx=65,pady=20,bg='grey',fg='blue',font=("arial",10,"bold"),command=button_mul)
+button_div=Button(root,text="/",padx=65,pady=20,bg='grey',fg='blue',font=("arial",10,"bold"),command=button_div)
+button_equal=Button(root,text="=",padx=65,pady=20,fg='blue',bg='black',command=button_equal)
+button_clear=Button(root,text="Clear",padx=125,pady=20,bg='red',command=button_clear)
+button_percentage=Button(root,text="%",padx=65,pady=20,bg='grey',fg='red',command=lambda:button_percentage)
 
 #putting buttons on the screen
 button_1.grid(row=3, column=0)
@@ -94,6 +103,7 @@ button_sub.grid(row=5,column=1)
 button_mul.grid(row=4,column=1)
 button_div.grid(row=5,column=0)
 button_equal.grid(row=5,column=2,columnspan=2)
+button_percentage.grid(row=6,column=2)
 
 root.mainloop()
 
